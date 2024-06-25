b638648a24034589936c0e9154d236f47037aecf
Max Dobeck <max.dobeck@evinced.com>
2024-06-25 16:05:39 -0400

    downgrade to resolve EventFiringDecorator error

--- src/test/java/com/maxdobeck/EvincedSetupTest.java
+++ src/test/java/com/maxdobeck/EvincedSetupTest.java
@@ -2,7 +2,7 @@ package com.maxdobeck;
 
 import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import com.evinced.EvincedWebDriver;
 import com.evinced.dto.results.Report;
 import com.evinced.EvincedSDK;
@@ -14,20 +14,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class EvincedSetupTest
-{
-    @Before
-    public void Setup()
-    {
-        EvincedSDK.setCredentials(System.getenv("SERVICE_ACCOUNT_ID"), System.getenv("API_KEY"));
-    }
-    
+{    
     // test for https://developer.evinced.com/sdks-for-web-apps/selenium-java-sdk#addevincedaccessibilitychecks(singlerunmode)
     @Test
     public void ShouldStartChrome() throws MalformedURLException
     {
         try {
-            WebDriver driver = new ChromeDriver();
-            EvincedWebDriver evincedDriver = new EvincedWebDriver(driver);
+            EvincedWebDriver evincedDriver = new EvincedWebDriver(new ChromeDriver());
+            EvincedSDK.setCredentials(System.getenv("SERVICE_ACCOUNT_ID"), System.getenv("API_KEY"));
             evincedDriver.get("https://www.google.com");
             Report report = evincedDriver.evAnalyze();
             // Assert that there are no accessibility issues
