f49a349d37b9239c338fc63cbbab7373b6e0f4f2
Max Dobeck <max.dobeck@evinced.com>
2024-07-08 16:49:01 -0400

    cleanup

--- src/test/java/com/maxdobeck/EvincedSetupTest.java
+++ src/test/java/com/maxdobeck/EvincedSetupTest.java
@@ -1,31 +1,31 @@
 package com.maxdobeck;
 
 import static org.junit.Assert.assertTrue;
-import org.openqa.selenium.*;
+// import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import com.evinced.EvincedWebDriver;
 import com.evinced.dto.results.Report;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import com.evinced.EvincedWebDriver;
 import com.evinced.EvincedSDK;
 import org.junit.Before;
 import org.junit.Test;
 import java.net.MalformedURLException;
 
 public class EvincedSetupTest
-{ 
+{
+    ChromeDriver driver;
+
     @Before
     public void setupAll() {
         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     // test for https://developer.evinced.com/sdks-for-web-apps/selenium-java-sdk#addevincedaccessibilitychecks(singlerunmode)
     @Test
-    public void ShouldStartChrome() throws MalformedURLException
-    {
+    public void ShouldStartChrome() throws MalformedURLException {
         try {
-            EvincedWebDriver evincedDriver = new EvincedWebDriver(new ChromeDriver());
+            EvincedWebDriver evincedDriver = new EvincedWebDriver(driver);
             EvincedSDK.setCredentials(System.getenv("SERVICE_ACCOUNT_ID"), System.getenv("API_KEY"));
             evincedDriver.get("https://demo.evinced.com");
             Report report = evincedDriver.evAnalyze();
