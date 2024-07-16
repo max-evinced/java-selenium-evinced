603d3ae4ff7c0a26e9c847124dc1abb1ccc10e3e
Max Dobeck <max.dobeck@evinced.com>
2024-07-16 13:14:39 -0400

    Simple test on Sauce Labs

--- src/test/java/com/maxdobeck/EvincedSauceTest.java
+++ src/test/java/com/maxdobeck/EvincedSauceTest.java
@@ -20,61 +20,10 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EvincedSetupTest
+public class EvincedSauceTest
 {
     ChromeDriver driver;
     EvincedWebDriver evincedDriver;
-
-    @Before
-    public void setupAll() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-    }
-
-    @After
-    public void teardownDrivers() {
-        if (driver != null) {
-            driver.quit();
-        }
-        if (evincedDriver != null) {
-            evincedDriver.quit();
-        }
-    }
-
-    // test for https://developer.evinced.com/sdks-for-web-apps/selenium-java-sdk#addevincedaccessibilitychecks(singlerunmode)
-    @Test
-    public void ShouldStartChrome() throws MalformedURLException {
-        try {
-            EvincedWebDriver evincedDriver = new EvincedWebDriver(driver);
-            EvincedSDK.setCredentials(System.getenv("SERVICE_ACCOUNT_ID"), System.getenv("API_KEY"));
-            evincedDriver.get("https://demo.evinced.com");
-            Report report = evincedDriver.evAnalyze();
-            // Assert that there are SOME accessibility issues
-            assertTrue(report.getIssues().size() != 0);
-        } catch (Exception ignore) {
-            // ignore exception
-            System.out.println(ignore);
-        }
-    }
-
-    @Test
-    public void SaveReports() throws MalformedURLException {
-        try {
-            EvincedWebDriver evincedDriver = new EvincedWebDriver(driver);
-            EvincedSDK.setCredentials(System.getenv("SERVICE_ACCOUNT_ID"), System.getenv("API_KEY"));
-            evincedDriver.get("https://demo.evinced.com");
-            Report landing_page_report = evincedDriver.evAnalyze();
-            EvincedReporter.evSaveFile("landing_page",landing_page_report, EvincedReporter.FileFormat.HTML);
-            assertTrue(landing_page_report.getIssues().size() != 0);
-
-            evincedDriver.get("https://demo.evinced.com/results/?what=Tiny%20House&where=Canada&date=Tue%20Jul%2009%202024%2011:21:39%20GMT-0400%20(Eastern%20Daylight%20Time");
-            takeSnapshot("results_page", evincedDriver);
-
-        } catch (Exception ignore) {
-            // ignore exception
-            System.out.println(ignore);
-        }
-    }
 
     @Test
     public void ChromeSauceTest() throws MalformedURLException {
@@ -86,8 +35,7 @@ public class EvincedSetupTest
             Map<String, Object> sauceOptions = new HashMap<>();
             sauceOptions.put("username", System.getenv("SAUCE_USERNAME"));
             sauceOptions.put("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
-            sauceOptions.put("build", "<your build id>");
-            sauceOptions.put("name", "<your test name>");
+            sauceOptions.put("name", "Simple Evinced Chrome Test");
             browserOptions.setCapability("sauce:options", sauceOptions);
 
             URL url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
@@ -99,6 +47,8 @@ public class EvincedSetupTest
             Report report = evincedDriver.evAnalyze();
             // Assert that there are SOME accessibility issues
             assertTrue(report.getIssues().size() != 0);
+            driver.executeScript("sauce:job-result=" + true);
+            driver.quit();
         } catch (Exception ignore) {
             // ignore exception
             System.out.println(ignore);
